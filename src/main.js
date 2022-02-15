724f85e873b62eb77fa1bde46305f6520664d25c
fanshuai <mygoodbean@163.com>
2022-02-15 14:43:51 +0800

    feate: 项目初始化的配置，引入第三方插件库

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import TDesign from 'tdesign-vue-next';
 import App from './App.vue'
+// 引入TDesign组件库全局样式资源
+import 'tdesign-vue-next/es/style/index.css';
+// 引入Tailwind全局样式资源
+import './styles'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).use(TDesign).mount('#app')
